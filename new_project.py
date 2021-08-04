4019f40caa6515d00a0666c16af4493f643077bf
rocketpy <dimitr.ushakov@gmail.com>
2021-08-05 00:06:31 +0300

    Update new_project.py

--- new_project.py
+++ new_project.py
@@ -1,12 +1,21 @@
+import numpy as np
+import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-import pandas as pd
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+#  Need Install: Dash, Pycountry, Plotly, and Pandas
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+df = pd.read_csv('file_name.csv', encoding="ISO-8859-1")
 
 
